64b217030de096dd3dc1c3952ea61dd5a9221fff
Aleksander <aleksievisa@gmail.com>
2024-02-19 14:45:26 +0300

    apple: auth test

--- app/page.tsx
+++ app/page.tsx
@@ -13,42 +13,42 @@ export default function Home() {
  
   return (
     <main>
-       <div className={styles.wrapper}>
-      <header className={styles.header}>
-        <button>
-          <ArrowLeft />
-        </button>
-        <h3>
-          {data?.user?.name || 'Создать Аккаунт'}
-        </h3>
-      </header>
-      <div className={styles.mail}>
-        <div className={styles.field}>
-          <span>Почта</span>
-          <Input />
-        </div>
-        <p>Вы получите ссылку приглашение на вашу почту</p>
-        <Button variant={'ghost'} size={'lg'}>
-          Создать
-        </Button>
-        <Button variant={'ghost'} size={'lg'} onClick={() => signOut()}>
-          Выйти
-        </Button>
-      </div>
-      <div className={styles.oauth}>
-        <span>
-          Создать профиль в один клик
-        </span>
-        <div>
-          <button onClick={() => signIn('google')}>
-            <IconGoogle />
-          </button>
+      <div className={styles.wrapper}>
+        <header className={styles.header}>
           <button>
-            <AppleIcon />
+            <ArrowLeft />
           </button>
+          <h3>
+            {data?.user?.name || 'Создать Аккаунт'}
+          </h3>
+        </header>
+        <div className={styles.mail}>
+          <div className={styles.field}>
+            <span>Почта</span>
+            <Input />
+          </div>
+          <p>Вы получите ссылку приглашение на вашу почту</p>
+          <Button variant={'ghost'} size={'lg'}>
+            Создать
+          </Button>
+          <Button variant={'ghost'} size={'lg'} onClick={() => signOut()}>
+            Выйти
+          </Button>
+        </div>
+        <div className={styles.oauth}>
+          <span>
+            Создать профиль в один клик
+          </span>
+          <div>
+            <button onClick={() => signIn('google')}>
+              <IconGoogle />
+            </button>
+            <button onClick={() => signIn('yandex')}>
+              <AppleIcon />
+            </button>
+          </div>
         </div>
       </div>
-    </div>
     </main>
   );
 }
--- auth.config.ts
+++ auth.config.ts
@@ -1,4 +1,5 @@
 import Google from "next-auth/providers/google"
+import Yandex from "next-auth/providers/yandex"
 
 import type { NextAuthConfig } from "next-auth"
 
@@ -7,6 +8,10 @@ export default {
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET
-    })
+    }),
+    Yandex({
+      clientId: process.env.YANDEX_CLIENT_ID,
+      clientSecret: process.env.YANDEX_CLIENT_SECRET
+    }),
   ],
 } satisfies NextAuthConfig
